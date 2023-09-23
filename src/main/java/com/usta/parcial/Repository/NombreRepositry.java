bea1134a38a4cbde63f59add6b37be071e3d6809
paulacamilamartinez <paula.martinezp@usantoto.edu.co>
2023-09-23 10:21:29 -0500

    Delete

--- src/main/java/com/usta/parcial/Repository/NombreRepositry.java
+++
@@ -1,7 +0,0 @@
-package com.usta.parcial.Repository;
-
-import com.usta.parcial.Entity.NombreEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NombreRepositry extends JpaRepository<NombreEntity, Long>  {
-}
